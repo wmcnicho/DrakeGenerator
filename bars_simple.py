f6256be406bb1fb48d5dec67ac35241d793970a0
William McNichols <wmcnicho@usc.edu>
2021-02-16 20:31:03 -0500

    Refactored bars_simple to enable save/load behavior. Implemented save and load and cleaned up some generate code.

--- bars_simple.py
+++ bars_simple.py
@@ -10,16 +10,13 @@ import matplotlib.pyplot as plt
 # Hyperparameters
 seq_length = 40
 embedding_dim = 256
-char_to_process = 10000 # Set to None to use all
+char_to_process = 5000 # Set to None to use all
 
 # Helper functions
 def split_input_target(sequence):
     input_text = sequence[:-1]
     target_text = sequence[1:]
     return input_text, target_text
-
-def text_from_ids(ids):
-  return tf.strings.reduce_join(chars_from_ids(ids), axis=-1)
 
 # Fancy graphs
 def plot_graphs(history, string):
@@ -28,13 +25,27 @@ def plot_graphs(history, string):
   plt.ylabel(string)
   plt.show()
 
+# Generate text
+def generate_text(seed_text, model, id_to_char_fn, chars_to_gen=300):
+  generated_text = seed_text
+  chars_from_ids = tf.keras.layers.experimental.preprocessing.StringLookup(vocabulary=id_to_char_fn.get_vocabulary(), invert=True)
+  for _ in range(chars_to_gen):    
+      input_id = id_to_char_fn(tf.strings.unicode_split(generated_text, 'UTF-8'))
+      padded_input_arr = pad_sequences([input_id], maxlen=seq_length, padding='pre')
+      padded_input = padded_input_arr[0]
+      prediction = model.predict_classes(padded_input)
+      str_from_ids = tf.strings.reduce_join(chars_from_ids(prediction), axis=-1)    
+      new_char = str_from_ids.numpy().decode('utf-8')[-1] # Jump through the conversion hoops and grab character
+      generated_text += new_char
+  return generated_text
+
 # Manually splitting up the dataset,
 # There is probably some built in methods to do this but why not recreate the wheel
 # returns a tuple of vectors to labels
-def split_data(input_data_arr, split_size, total_splits=None):
+def split_data(input_data_arr, vocab_size, seq_length, total_splits=None):
   total_chars = len(input_data_arr)
   if total_splits is None:
-    total_splits = total_chars-split_size-1
+    total_splits = total_chars-seq_length-1
   agg_xs = []
   agg_ys = []
   for i in range(0, total_splits, seq_length): # We could remove the step for more training data
@@ -53,62 +64,81 @@ def split_data(input_data_arr, split_size, total_splits=None):
   numpy_ys = np.array(agg_ys)
   return (numpy_xs, numpy_ys)
 
-## Open and pre-process the data
-# Verified this works with alphabet now lets make things more interesting
-# data = open('./archive/alphabet.txt').read()
-data = open('./archive/drake_lyrics.txt').read()
-print('Length of text: {} characters'.format(len(data)))
+def main(do_train=True):
+  ## Open and pre-process the data
+  # Verified this works with alphabet now lets make things more interesting
+  # data = open('./archive/alphabet.txt').read()
+  data = open('./archive/drake_lyrics.txt').read()
+  print('Length of text: {} characters'.format(len(data)))
 
-vocab = sorted(set(data))
+  vocab = sorted(set(data))
 
-# This function as variable setup is weird to me but whatever
-ids_from_chars = preprocessing.StringLookup(vocabulary=list(vocab))
-chars_from_ids = tf.keras.layers.experimental.preprocessing.StringLookup(vocabulary=ids_from_chars.get_vocabulary(), invert=True)
+  # This function as variable setup is weird to me but whatever
+  ids_from_chars = preprocessing.StringLookup(vocabulary=list(vocab))
+  chars_from_ids = tf.keras.layers.experimental.preprocessing.StringLookup(vocabulary=ids_from_chars.get_vocabulary(), invert=True)
 
-# Preprocess the text into characters
-all_ids = ids_from_chars(tf.strings.unicode_split(data, 'UTF-8'))
-vocab_size = len(ids_from_chars.get_vocabulary())
+  # Preprocess the text into characters
+  all_ids = ids_from_chars(tf.strings.unicode_split(data, 'UTF-8'))
+  vocab_size = len(ids_from_chars.get_vocabulary())
 
-# Output vocab mapping for sanity
-vocab_sample = list(range(0,vocab_size))
-tf_vocab = tf.convert_to_tensor(vocab_sample)
-mapped_vocab = chars_from_ids(tf_vocab).numpy()
-print(vocab_sample)
-print(mapped_vocab)
+  # Output vocab mapping for sanity
+  vocab_sample = list(range(0,vocab_size))
+  tf_vocab = tf.convert_to_tensor(vocab_sample)
+  mapped_vocab = chars_from_ids(tf_vocab).numpy()
+  print(vocab_sample)
+  print(mapped_vocab)
 
-(split_xs, split_ys) = split_data(all_ids.numpy(), seq_length, 50000)
+  if do_train:
+    (split_xs, split_ys) = split_data(all_ids.numpy(), vocab_size, seq_length, char_to_process)
 
-## Build the model
-model = tf.keras.models.Sequential()
-model.add(tf.keras.layers.Embedding(vocab_size, embedding_dim, input_length=seq_length))
-model.add(tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(150)))
-model.add(tf.keras.layers.Dense(vocab_size, activation='softmax'))
-print(model.summary())
+    ## Build the model
+    model = tf.keras.models.Sequential()
+    model.add(tf.keras.layers.Embedding(vocab_size, embedding_dim, input_length=seq_length))
+    model.add(tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(150)))
+    model.add(tf.keras.layers.Dense(vocab_size, activation='softmax'))
+    print(model.summary())
 
-adam = tf.keras.optimizers.Adam(lr=0.01)
-model.compile(loss='categorical_crossentropy', optimizer=adam, metrics=['accuracy'])
+    adam = tf.keras.optimizers.Adam(lr=0.01)
+    model.compile(loss='categorical_crossentropy', optimizer=adam, metrics=['accuracy'])
 
-## Train the model
-#earlystop = EarlyStopping(monitor='val_loss', min_delta=0, patience=5, verbose=0, mode='auto')
-history = model.fit(x=split_xs, y=split_ys, epochs=10, verbose=1)
-# Uncomment to show dope graph
-#plot_graphs(history, 'accuracy')
+    ## Train the model
+    #earlystop = EarlyStopping(monitor='val_loss', min_delta=0, patience=5, verbose=0, mode='auto')
+    history = model.fit(x=split_xs, y=split_ys, epochs=10, verbose=1)
+    # Uncomment to show dope graph
+    #plot_graphs(history, 'accuracy')
+
+    saved_model_dir = "./models/simple_model/"
+    model_filename = 'simple_bars.h5'
+    model.save(saved_model_dir+model_filename)
+  else:
+    print("Loading model from file.")
+    model = tf.keras.models.load_model("./models/simple_model/simple_bars.h5")
 
 
-## Generate text with model
-# Dank so we trained for 50 iterations on a slice of the data
-# Let's see what this model generates for a few seeds
-seed_text = '[Verse]\n'
-chars_to_gen = 300
-generated_text = seed_text
+  ## Generate text with model
+  # Dank so we trained for 50 iterations on a slice of the data
+  # Let's see what this model generates for a few seeds
+  num_chars = 100
+  seed_text = '[Verse]\n'
+  output_text = generate_text(seed_text, model, ids_from_chars, chars_to_gen=num_chars)
+  print("Input sequence was: %s" % (seed_text))
+  print("%d character generated sequence:\n%s" % (num_chars, output_text))
 
-for _ in range(chars_to_gen):    
-    input_id = ids_from_chars(tf.strings.unicode_split(generated_text, 'UTF-8'))
-    padded_input_arr = pad_sequences([input_id], maxlen=seq_length, padding='pre')
-    padded_input = padded_input_arr[0]
-    prediction = model.predict_classes(padded_input)
-    new_char = text_from_ids(prediction).numpy().decode('utf-8')[-1] # Jump through the conversion hoops and grab character
-    generated_text += new_char
+  seed_text = 'boy'
+  output_text = generate_text(seed_text, model, ids_from_chars, chars_to_gen=num_chars)
+  print("Input sequence was: %s" % (seed_text))
+  print("%d character generated sequence:\n%s" % (num_chars, output_text))
 
-print("Input sequence was: %s" % (seed_text))
-print("%d character generated sequence:\n%s" % (chars_to_gen, generated_text))
+  seed_text = 'you'
+  output_text = generate_text(seed_text, model, ids_from_chars, chars_to_gen=num_chars)
+  print("Input sequence was: %s" % (seed_text))
+  print("%d character generated sequence:\n%s" % (num_chars, output_text))
+
+  seed_text = 'love'
+  output_text = generate_text(seed_text, model, ids_from_chars, chars_to_gen=num_chars)
+  print("Input sequence was: %s" % (seed_text))
+  print("%d character generated sequence:\n%s" % (num_chars, output_text))
+
+
+if __name__ == "__main__":
+    main(do_train=False)
