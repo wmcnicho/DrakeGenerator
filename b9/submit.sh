70390a2729d461de3318d3cb14f03f661e0f9450
William McNichols <wmcnicho@usc.edu>
2021-04-22 18:26:18 -0400

    Stubbed out b9 bash script

---
+++ b9/submit.sh
@@ -0,0 +1,8 @@
+# Local command:
+# docker  run  -v `pwd`:`pwd` -w `pwd` -i -t tensorflow/tensorflow python3 bars_custom.py epochs=8 cell_type=gru train=t
+
+#git clone https://github.com/wmcnicho/DrakeGenerator.git
+
+#pip install -r ./requirements.txt
+
+#python3 bars_custom.py epochs=8 cell_type=gru train=t
