b5b7c214dabb4720100fef64369fae7e7444fd75
William McNichols <wmcnicho@usc.edu>
2021-04-23 17:19:17 -0400

    Removing deprecated branch from submission script

--- b9/slippi-run.sh
+++ b9/slippi-run.sh
@@ -1,6 +1,5 @@
 export DRAKEMONGOURL='mongodb+srv://hunter:HunterC0dez!@drakecluster.wj44a.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 git clone https://github.com/wmcnicho/DrakeGenerator.git
 cd DrakeGenerator
-git checkout sacredSetup
 pip3 install -r requirements.txt
 python3 bars_custom.py with cell_type=gru num_epochs=1000 batch_size=256
