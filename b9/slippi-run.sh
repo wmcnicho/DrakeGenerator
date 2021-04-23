4e83fd022f611fe444521f2fdcc3585d4ca93447
William McNichols <wmcnicho@usc.edu>
2021-04-23 17:17:45 -0400

    Latest version of the b9 script

--- b9/slippi-run.sh
+++ b9/slippi-run.sh
@@ -3,4 +3,4 @@ git clone https://github.com/wmcnicho/DrakeGenerator.git
 cd DrakeGenerator
 git checkout sacredSetup
 pip3 install -r requirements.txt
-python3 bars_custom.py with cell_type=gru num_epochs=5
+python3 bars_custom.py with cell_type=gru num_epochs=1000 batch_size=256
